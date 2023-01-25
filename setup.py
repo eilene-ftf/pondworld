cf387328ba587a2c3d0745c5a5f610a98b91a249
Eilene <eileneftf@gmail.com>
2023-01-25 06:03:14 -0500

    added tk to required packages

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from setuptools import setup
 
 setup(
         name='pondworld',
-        version='0.0.4',
+        version='0.0.5',
         description='A little minigrid world featuring a frog that eats flies',
         author='Eilene Tomkins-Flanagan',
         author_email='eilenetomkinsflanaga@cmail.carleton.ca',
         packages=['pondworld'],
-        install_requires=['gymnasium', 'numpy', 'cairosvg', 'opencv-python', 'minigrid'],
+        install_requires=['gymnasium', 'numpy', 'cairosvg', 'opencv-python', 'minigrid', 'tk'],
 
         classifiers=[
             'Programming Language :: Python :: 3'
