210f99deaf9d841a8fb0427901e95de450b90e94
Eilene <eileneftf@gmail.com>
2023-01-25 06:05:56 -0500

    added envs to setup.py

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from setuptools import setup
 
 setup(
         name='pondworld',
-        version='0.0.5',
+        version='0.0.6',
         description='A little minigrid world featuring a frog that eats flies',
         author='Eilene Tomkins-Flanagan',
         author_email='eilenetomkinsflanaga@cmail.carleton.ca',
-        packages=['pondworld'],
+        packages=['pondworld', 'pondworld.envs'],
         install_requires=['gymnasium', 'numpy', 'cairosvg', 'opencv-python', 'minigrid', 'tk'],
 
         classifiers=[
