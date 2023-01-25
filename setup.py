c44e7d55837d813521b8c35089985eaf1d4a3316
Eilene <eileneftf@gmail.com>
2023-01-25 06:07:07 -0500

    added assets to setup.py

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from setuptools import setup
 
 setup(
         name='pondworld',
-        version='0.0.6',
+        version='0.0.7',
         description='A little minigrid world featuring a frog that eats flies',
         author='Eilene Tomkins-Flanagan',
         author_email='eilenetomkinsflanaga@cmail.carleton.ca',
-        packages=['pondworld', 'pondworld.envs'],
+        packages=['pondworld', 'pondworld.envs', 'pondworld.assets'],
         install_requires=['gymnasium', 'numpy', 'cairosvg', 'opencv-python', 'minigrid', 'tk'],
 
         classifiers=[
