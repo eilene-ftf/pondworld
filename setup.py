2a8f827f403940c8ec4519e44fa5d3df0555318d
Eilene <eileneftf@gmail.com>
2023-01-25 06:22:49 -0500

    tried moving assets around again and jiggling setup.py, MANIFEST.in to shake something out

--- setup.py
+++ setup.py
@@ -2,11 +2,12 @@ from setuptools import setup
 
 setup(
         name='pondworld',
-        version='0.0.10',
+        version='0.0.11',
         description='A little minigrid world featuring a frog that eats flies',
         author='Eilene Tomkins-Flanagan',
         author_email='eilenetomkinsflanaga@cmail.carleton.ca',
-        packages=['pondworld', 'pondworld.envs', 'pondworld.assets'],
+        include_package_data=True,
+        packages=['pondworld', 'pondworld.envs', 'assets'],
         install_requires=['gymnasium', 'numpy', 'cairosvg', 'opencv-python', 'minigrid', 'tk'],
 
         classifiers=[
