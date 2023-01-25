07a2c10ae4e1743628e97683c40f4f2aaf571e74
Eilene <eileneftf@gmail.com>
2023-01-25 07:37:05 -0500

    increment version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
         name='pondworld',
-        version='0.0.12',
+        version='0.0.13',
         description='A little minigrid world featuring a frog that eats flies',
         author='Eilene Tomkins-Flanagan',
         author_email='eilenetomkinsflanaga@cmail.carleton.ca',
