fe525733420c8cba8f0bf4afe8514acd5520f545
Eilene <eileneftf@gmail.com>
2023-02-06 05:01:25 -0500

    added a frog cam and viewfinder to look, appropriate state values for object colours

--- pondworld/frog_control/frog_control.py
+++ pondworld/frog_control/frog_control.py
@@ -5,7 +5,7 @@ import gymnasium as gym
 from minigrid.minigrid_env import MiniGridEnv
 from minigrid.utils.window import Window
 from minigrid.wrappers import ImgObsWrapper, RGBImgPartialObsWrapper
-from minigrid.core.constants import OBJECT_TO_IDX
+from minigrid.core.constants import OBJECT_TO_IDX, COLOR_TO_IDX, STATE_TO_IDX
 
 from minigrid.manual_control import ManualControl
 
@@ -17,10 +17,26 @@ import numpy as np
 from minigrid.core.world_object import Key, Door
 from minigrid.core.world_object import Box as Fly
 
+code_obj = 0
+code_col = 20
+code_state = 30
+code_sep = 40
+codes = (code_obj, code_col, code_state)
+
 OBJECT_TO_IDX = OBJECT_TO_IDX.copy()
 OBJECT_TO_IDX["fly"] = 7
 OBJECT_TO_IDX["lit"] = 11
+OBJECT_TO_IDX["viewfinder"] = 0
 del OBJECT_TO_IDX["box"]
+
+for k, col in COLOR_TO_IDX.items():
+    OBJECT_TO_IDX[k] = col + code_col
+    
+for k, state in STATE_TO_IDX.items():
+    OBJECT_TO_IDX[k] = state + code_state
+    
+OBJECT_TO_IDX["separator"] = code_sep
+
 IDX_TO_OBJECT = {v: k for k, v in OBJECT_TO_IDX.items()}
 
 WORLD_OBJECTS = {'key':  Key, 
@@ -63,6 +79,16 @@ class FrogControl(ManualControl):
 
         self.textmode = textmode
         self.emojis = emojis
+        
+        self.jc = '' if self.emojis else ' '
+        self.col_idxs = list(COLOR_TO_IDX.values()).sort() # r, g, b, p, y, w
+        self.cols = ('🟥', '🟩', '🟦', '🟪', '🟨', '⬜') if self.emojis else ('R', 'G', 'B', 'P', 'Y', 'W')
+        self.states = ('⬛', '🔓', '🔒') if self.emojis else (' ', 'U', 'L')
+        self.obj_idxs = [OBJECT_TO_IDX[k] for k in ('fly', 'empty', 'wall', 'agent', 'lit', 'door', 'key', 'viewfinder', 'red', 'green', 'blue', 'yellow', 'purple', 'grey', 'open', 'closed', 'locked', 'separator')]
+        self.objects = ('🪰', '⬛', '🧱', '🐸', '🟨', '🚪', '🗝️', '⬛') if self.emojis else ('°',' ','#','♦', '_', '▥', '🗝', ' ')
+        self.all_strs = self.objects + self.cols + self.states + ('💠',) if self.emojis else ('=',) 
+        self.obj_str = {k: v for k, v in zip(self.obj_idxs, self.all_strs)}
+        self.dirs = ['⬇️', '➡️', '⬆️', '⬅️',] if self.emojis else ['🖣', '☛', '🖢', '☚']
         
     def start(self):
         self.reset(self.seed)
@@ -116,8 +142,6 @@ class FrogControl(ManualControl):
     def look(self):
         if self.textmode:
             #print("\033c", end='')
-            
-            # This needs to be used to process occlusion eventually
             _, view = self.env.gen_obs_grid()
             
             viewrot = np.rot90(view, k=self.env.agent_dir+1)
@@ -170,23 +194,58 @@ class FrogControl(ManualControl):
                     ylit = y - min(0, exts[self.env.agent_dir][0])
                     xlit = x - min(0, exts[self.env.agent_dir][2])
                     
-                    #print(f'[{ylit} {xlit}]')
                 
                     if world[i, j] == 1 and viewrot[ylit, xlit]:
                         world[i, j] = 11
             
-            jc = '' if self.emojis else ' '
-            idxs = [OBJECT_TO_IDX[k] for k in ('fly', 'empty', 'wall', 'agent', 'lit', 'door', 'key')]
-            objects = ('🪰', '⬛', '🧱', '🐸', '🟨', '🚪', '🗝️') if self.emojis else ('°',' ','#','♦', '_', '▥', '%')
-            s = {k: v for k, v in zip(idxs, objects)}
-            for row in world:
-                print(jc.join([s[t] for t in row]))
+            cam_image = self.obs['image'][:, :, 0] * view
+            
+            # Wondering if I should print the colour of grey walls too
+            for i, j in np.array(np.where(cam_image == OBJECT_TO_IDX['wall'])).T:
+                if self.obs['image'][i, j, 1] != OBJECT_TO_IDX['grey'] - code_col:
+                    cam_image[i, j] = self.obs['image'][i, j, 1] + code_col
+            
+            frog_cam = np.zeros((world.shape[0], view.shape[1]), dtype=int)
+            frog_cam[world.shape[0]-view.shape[1]:, :] = np.rot90(cam_image, k=-1)
+            
+            for i, j in np.array(np.where(frog_cam == OBJECT_TO_IDX['empty'])).T:
+                frog_cam[i, j] = OBJECT_TO_IDX['lit']
+                
+            
+            cam_break = world.shape[0]-view.shape[1]
+            
+            for i, j in np.array(np.where(frog_cam[world.shape[0]-view.shape[1]:, :] == 0)).T:
+                frog_cam[cam_break+i, j] = OBJECT_TO_IDX['empty']
+                
+            frog_cam[-1, frog_cam.shape[1]//2] = OBJECT_TO_IDX['agent']
+            
+            
+            viewfinder = np.zeros((cam_break-1, view.shape[0]))
+            vo = 0
+            for vo, (i, j) in enumerate(np.array(np.where(sum(OBJECT_TO_IDX[k] == cam_image for k in ('fly', 'key', 'door')))).T):
+                viewfinder[-(vo//2) - 1, vo%2*4:vo%2*4+3] = self.obs['image'][i, j, :] + codes
+                
+            frog_cam[:cam_break-1, :] = viewfinder
+            
+            frog_cam[cam_break-1, :] = code_sep
+            
+            for i, (world_row, cam_row) in enumerate(zip(world, frog_cam)):
+                cam_str = self.jc.join(self.obj_str[t] for t in cam_row)
+                
+                if i < cam_break - 1:
+                    if (frog_cam[i+1] != OBJECT_TO_IDX['separator']).any() and (frog_cam[i+1] != OBJECT_TO_IDX['viewfinder']).any():
+                        cam_str = 'viewfinder:'
+                    elif (cam_row == OBJECT_TO_IDX['viewfinder']).all():
+                        cam_str = ''
+                    
+                if i == cam_break - 1:
+                    cam_str = 'live frog reaction:'
+                print(self.jc.join(self.obj_str[t] for t in world_row) + '    ' + cam_str)
                 
             print(self.env.mission)
             
-            dirs = ['⬇️', '➡️', '⬆️', '⬅️',] if self.emojis else ['🖣', '☛', '🖢', '☚']
             
-            print(f'frog compass: {dirs[self.env.agent_dir]}')
+            print(f'frog compass: {self.dirs[self.env.agent_dir]}')
 
             
         state = {}
@@ -197,11 +256,14 @@ class FrogControl(ManualControl):
                 
             if self.obs['image'][self.my_coord[0], -2, 0] == OBJECT_TO_IDX[obj]:
                 state[obj] = 1  # the object is directly in front
+                state[f'{obj}_colour'] = IDX_TO_OBJECT[self.obs['image'][self.my_coord[0], -2, 1] + code_col]
                 continue
 
             objs = np.argwhere(view*self.obs['image'][:, :, 0] == OBJECT_TO_IDX[obj])
             closest = np.argmin(map(self._taxicab, objs)) if objs.any() else None
-
+            
+            #state[f'{obj}_colour']
+                                                       
             if closest is None: 
                 state[obj] = 0 # there are no objects
                 continue
@@ -215,6 +277,6 @@ class FrogControl(ManualControl):
             else: # dist_x < 0
                 state[obj] = 4 # fly is farther to the right
                 
-           
-        #print(state)
+            state[f'{obj}_colour'] = IDX_TO_OBJECT[self.obs['image'][(*objs[closest], 1)] + code_col]
+                
         return state
